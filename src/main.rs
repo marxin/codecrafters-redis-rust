74cbf1187478da614221affccc5acd22dda2c0d5
Martin Liska <martin.liska@hey.com>
2024-05-03 21:13:45 +0200

    Implement basic parsing and async server

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,44 @@
-// Uncomment this block to pass the first stage
-// use std::net::TcpListener;
+use tokio::io::{AsyncWriteExt, BufReader};
+use tokio::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+use crate::parser::{serialize, RedisValue};
 
-    // Uncomment this block to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+mod parser;
+
+const ADDR: &str = "127.0.0.1:6379";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let listener = TcpListener::bind(ADDR).await?;
+    println!("Listening on: {}", ADDR);
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut socket = std::pin::pin!(BufReader::new(socket));
+            let token_result = parser::parse_token(&mut socket).await;
+            match token_result {
+                Ok(value) => match value {
+                    RedisValue::Array(items) => {
+                        for item in items {
+                            match item {
+                                RedisValue::String(command) => match &command.as_str() {
+                                    &"ping" => {
+                                        let reply =
+                                            serialize(RedisValue::String("PONG".to_string()));
+                                        socket.write_all(reply.as_bytes()).await;
+                                    }
+                                    _ => todo!(),
+                                },
+                                _ => todo!("unsupported command"),
+                            }
+                        }
+                    }
+                    _ => todo!("only array replies supported now"),
+                },
+                Err(err) => eprintln!("Cannot parse token: {:?}", err),
+            }
+        });
+    }
 }
---
+++ src/parser.rs
@@ -0,0 +1,85 @@
+use std::pin::Pin;
+
+use anyhow::Context;
+use tokio::io::{AsyncBufRead, AsyncBufReadExt, AsyncReadExt};
+
+const SEPARATOR: &[u8; 2] = b"\r\n";
+const SEPARATOR_STRING: &str = "\r\n";
+
+#[derive(Debug)]
+pub enum RedisValue {
+    String(String),
+    Array(Vec<RedisValue>),
+}
+
+async fn read_n<R: AsyncBufRead>(reader: &mut Pin<&mut R>, n: usize) -> anyhow::Result<Vec<u8>> {
+    let mut buffer = vec![0u8; n];
+    reader
+        .read_exact(&mut buffer)
+        .await
+        .with_context(|| format!("Cannot read {n} bytes from the input"))?;
+    Ok(buffer)
+}
+
+async fn next_char<R: AsyncBufRead>(reader: &mut Pin<&mut R>) -> anyhow::Result<u8> {
+    read_n(reader, 1)
+        .await?
+        .into_iter()
+        .next()
+        .ok_or(anyhow::anyhow!("a non empty string expected"))
+}
+
+async fn next_part<R: AsyncBufRead>(reader: &mut Pin<&mut R>) -> anyhow::Result<String> {
+    let mut buffer = Vec::new();
+    reader.read_until(SEPARATOR[0], &mut buffer).await?;
+    anyhow::ensure!(next_char(reader).await? == SEPARATOR[1]);
+
+    // pop the trailing separator
+    buffer.pop().ok_or(anyhow::anyhow!("pop trailing char"))?;
+    Ok(String::from_utf8(buffer)?)
+}
+
+pub async fn parse_token<R: AsyncBufRead>(reader: &mut Pin<&mut R>) -> anyhow::Result<RedisValue> {
+    let start_letter = next_char(reader).await?;
+
+    match start_letter {
+        b'$' => {
+            let length = next_part(reader).await?.parse::<usize>()?;
+            let value = read_n(reader, length).await?;
+            anyhow::ensure!(&read_n(reader, 2).await? == SEPARATOR);
+            Ok(RedisValue::String(String::from_utf8(value)?))
+        }
+        b'*' => {
+            let element_count = next_part(reader).await?.parse::<usize>()?;
+            let mut elements = Vec::new();
+            for _ in 0..element_count {
+                elements.push(Box::pin(parse_token(reader)).await?);
+            }
+            Ok(RedisValue::Array(elements))
+        }
+        _ => anyhow::bail!(format!(
+            "Unsupported leading character: '{}'",
+            start_letter as char
+        )),
+    }
+}
+
+pub fn serialize(value: RedisValue) -> String {
+    match value {
+        RedisValue::String(value) => {
+            format!(
+                "${}{SEPARATOR_STRING}{value}{SEPARATOR_STRING}",
+                value.len()
+            )
+        }
+        RedisValue::Array(array) => {
+            let length = array.len();
+            let content = array
+                .into_iter()
+                .map(serialize)
+                .collect::<Vec<_>>()
+                .join("");
+            format!("*{length}{SEPARATOR_STRING}{content}{SEPARATOR_STRING}")
+        }
+    }
+}
