2f8b20e6766f59a659e89e8b95e2157c9f357906
Martin Liska <martin.liska@hey.com>
2024-05-06 19:05:02 +0200

    Replication: add --port argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::net::SocketAddr;
 use std::sync::{Arc, Mutex};
 
+use clap::Parser;
 use tokio::io::{AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 
@@ -9,12 +11,21 @@ use crate::server::RedisServer;
 mod parser;
 mod server;
 
-const ADDR: &str = "127.0.0.1:6379";
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Port to listed to
+    #[arg(short, long, default_value_t = 6379)]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let listener = TcpListener::bind(ADDR).await?;
-    println!("Listening on: {}", ADDR);
+    let args = Args::parse();
+    let addr = SocketAddr::from(([127, 0, 0, 1], args.port));
+
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on: {}", addr);
     let server = Arc::new(Mutex::new(RedisServer::new()));
 
     loop {
